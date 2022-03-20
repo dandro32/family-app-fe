8f6a5ef4d2c31dd144a438d9cbd26b115f2bb582
Daniel <daniel.drozdzel@vattenfall.com>
2022-03-20 21:13:20 +0100

    material-ui

---
+++ pages/src/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+import { red, blue, deepPurple } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blue[500],
+    },
+    secondary: {
+      main: deepPurple[200],
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+});
+
+export default theme;
